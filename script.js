c08c690c123248013b08336e98a4c486fb069acf
LitleStuart <mukhorinvanga@gmail.com>
2021-04-30 01:38:30 +0500

    added counter of items in cart

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
+const cartBadge = document.getElementById('cartBadge');
+let countItemsInCart = 0;
+
 function addedToCart() {
     Swal.fire({
         title: 'Added to cart',
         icon: 'success',
         timer: 5000,
         confirmButtonColor: '#009688'
-    })
+    });
+    countItemsInCart+=1;
+    cartBadge.setAttribute('data-badge', countItemsInCart);
 }
 
 function cart() {
     Swal.fire({
         titleText: 'YOUR CART',
         icon: 'info',
-        timer: 5000,
-        confirmButtonColor: '#009688'
+        timer: 10000,
+        text: 'There are ' + countItemsInCart + ' items!',
+        confirmButtonColor: '#009688',
+        showCancelButton: true,
+        reverseButtons: true
     })
 }

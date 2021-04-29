f7e4d296c04f02d0dd9c96f1f44762594f0a9de6
LitleStuart <mukhorinvanga@gmail.com>
2021-04-30 00:32:52 +0500

    added 'cart' button

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ function addedToCart() {
     Swal.fire({
         title: 'Added to cart',
         icon: 'success',
-        timer: 10000
+        timer: 5000,
+        confirmButtonColor: '#009688'
+    })
+}
+
+function cart() {
+    Swal.fire({
+        titleText: 'YOUR CART',
+        icon: 'info',
+        timer: 5000,
+        confirmButtonColor: '#009688'
     })
 }
